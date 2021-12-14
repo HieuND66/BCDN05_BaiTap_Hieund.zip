51bcdd54380d5d857aeccdd126e6759ef8b9b3dd
Hieund <ngoduyhieu66@gmail.com>
2021-12-14 10:21:04 +0700

    js buổi 4 function

--- js_b4/js/main.js
+++ js_b4/js/main.js
@@ -15,7 +15,6 @@
  *Khối 3:
  *total và kết quả đậu hay rớt
 */
-
 function tuyenSinh() {
   var diemChuan = Number(document.getElementById('inpScores').value);
   var mon1 = Number(document.getElementById('inpScores1').value);
